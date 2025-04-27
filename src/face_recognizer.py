1d7557fe1ec93f9c11e92b494a2bfda59af4c2ad
Elif Vural <elifvural019@gmail.com>
2025-04-27 09:32:07 +0300

    known faces added

--- src/face_recognizer.py
+++ src/face_recognizer.py
@@ -1,12 +1,28 @@
 import numpy as np
+import json
+import os
 from deepface import DeepFace
 
-
-#yüz tanıma ve isimlendirme
 class FaceRecognizer:
-    def __init__(self):
-        self.known_faces = {}  # Yüz vektörleri ve isimler
+    def __init__(self, embeddings_file="../data/known_faces.json"):
+        self.embeddings_file = embeddings_file
+        self.known_faces = {}
         self.face_id_counter = 0
+
+        # Önceki vektörleri dosyadan yükle (varsa)
+        if os.path.exists(embeddings_file):
+            with open(embeddings_file, 'r') as f:
+                loaded_data = json.load(f)
+                self.known_faces = {tuple(map(float, k.split(','))): v for k, v in loaded_data.items()}
+                # Son kişi ID'sini bul
+                if self.known_faces:
+                    self.face_id_counter = max(ord(name[-1]) - 64 for name in self.known_faces.values())
+
+    def save_embeddings(self):
+        # Vektörleri dosyaya kaydet (anahtarları string'e çevir)
+        embeddings_to_save = {','.join(map(str, k)): v for k, v in self.known_faces.items()}
+        with open(self.embeddings_file, 'w') as f:
+            json.dump(embeddings_to_save, f)
 
     def recognize_face(self, face):
         try:
@@ -28,5 +44,6 @@ class FaceRecognizer:
             recognized_name = f"Kişi {chr(64 + self.face_id_counter)}"  # A, B, C...
             self.known_faces[tuple(embedding)] = recognized_name
             print(f"Yeni yüz kaydedildi: {recognized_name}")
+            self.save_embeddings()  # Yeni yüzü dosyaya kaydet
 
         return recognized_name
